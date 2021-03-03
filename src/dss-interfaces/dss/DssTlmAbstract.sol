7d2930e9d50da7cfb952d48e68ddd2e10cad6787
Yash Sinha <7387135+ysinha1@users.noreply.github.com>
2021-03-03 09:28:05 -0500

    TLM interface ready

--- src/dss-interfaces/dss/DssTlmAbstract.sol
+++ src/dss-interfaces/dss/DssTlmAbstract.sol
@@ -1,15 +1,10 @@
 pragma solidity ^0.6.10;
-import "./AuthGemJoinAbstract.sol";
-import "./DaiJoinAbstract.sol";
-
 
 interface DssTlmAbstract {
-    //todo: remove unneeded functions here
-    function gemJoin() external view returns(AuthGemJoinAbstract);
-    function daiJoin() external view returns(DaiJoinAbstract);
-    function tin() external view returns(uint256);
-    function tout() external view returns(uint256);
-    function file(bytes32 what, uint256 data) external;
-    function sellGem(address usr, uint256 gemAmt) external;
-    function buyGem(address usr, uint256 gemAmt) external;
+    function init(bytes32 ilk, address gemJoin, uint256 yield) external;
+    function file(bytes32 ilk, bytes32 what, uint256 data) external;
+    function hope(address usr) external;
+    function nope(address usr) external;
+    function sellGem(bytes32 ilk, address usr, uint256 gemAmt) external;
+    function buyGem(bytes32 ilk, address usr, uint256 amt) external;
 }
