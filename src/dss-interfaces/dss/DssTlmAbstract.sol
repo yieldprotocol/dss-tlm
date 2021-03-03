cdcd753cac1845c1fbe927b4c960c3c0a7110945
Yash Sinha <7387135+ysinha1@users.noreply.github.com>
2021-03-03 17:05:17 -0500

    add extra state variables and funcs to tlm interface

--- src/dss-interfaces/dss/DssTlmAbstract.sol
+++ src/dss-interfaces/dss/DssTlmAbstract.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.6.10;
 
 interface DssTlmAbstract {
+	  mapping (address => uint256) public wards;
+    function rely(address usr) external auth { wards[usr] = 1; emit Rely(usr); }
+    function deny(address usr) external auth { wards[usr] = 0; emit Deny(usr); }
+    VatAbstract immutable public vat;
+    DaiJoinAbstract immutable public daiJoin;
+    DaiAbstract immutable public dai;
+    address immutable public vow;
+    mapping (bytes32 => Ilk) public ilks; // Registered maturing gems
+
     function init(bytes32 ilk, address gemJoin, uint256 yield) external;
     function file(bytes32 ilk, bytes32 what, uint256 data) external;
     function hope(address usr) external;
